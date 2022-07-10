8ef6af4211ad7baec10bda943a79198f7e89fa5d
quanbui210 <89993167+quanbui210@users.noreply.github.com>
2022-07-10 23:25:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-
 const form = document.querySelector('#searchForm');
+let imgContainer = document.querySelector(".img-container");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
     // const config = {params: {q: searchTerm}}
     const searchTerm = form.elements.query.value
-    const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    const config = {params: {q: searchTerm}}
+    const response = await axios.get(`https://api.tvmaze.com/search/shows`, config)
+    form.elements.query.value = "";
     displayImage(response.data)
 })  
 
-const displayImage = function (shows) {
+const displayImage = (shows) => {
+    imgContainer.innerHTML = "";
     for (let result of shows) {
         if(result.show.image) {
             const img = document.createElement("IMG");
             img.src = result.show.image.medium
-            document.body.append(img);
+            imgContainer.append(img);
             img.classList.add("img-style")
            } 
         }
